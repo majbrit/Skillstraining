2d52772abe8b2aba80f0b99e011b2ff1f60fa6a9
majbrit <majbrit.schoettner@yahoo.de>
2023-01-15 18:47:44 +0100

    hover repariert

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -6,17 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  selected: string ='book-nav';
+  selected: string ='';
+
   select(navItem:string){
 
-    (<HTMLInputElement>document.getElementById(this.selected)).style.backgroundColor="white";
-    (<HTMLInputElement>document.getElementById(this.selected)).style.color="rgb(82, 82, 82)";
+    console.log("select");
+    /*
+    if(this.selected != null && this.selected != ''){
+      console.log("select neeein");
+      (<HTMLInputElement>document.getElementById(this.selected)).classList.remove("focused-button");
+      (<HTMLInputElement>document.getElementById(this.selected)).classList.add("notfocused-button");
+    }
+
+    (<HTMLInputElement>document.getElementById(navItem)).classList.remove("notfocused-button");
+    (<HTMLInputElement>document.getElementById(navItem)).classList.add("focused-button");
+    */
+
+    if(this.selected != null && this.selected != ''){
+      (<HTMLInputElement>document.getElementById(this.selected)).style.backgroundColor="white";
+      (<HTMLInputElement>document.getElementById(this.selected)).style.color="rgb(82, 82, 82)";
+    }
     (<HTMLInputElement>document.getElementById(navItem)).style.backgroundColor="orangered";
     (<HTMLInputElement>document.getElementById(navItem)).style.color="white";
     this.selected = navItem;
   }
+
+  hover(navItem:string){
+
+    console.log("hover");
+    if(navItem != this.selected){
+      (<HTMLInputElement>document.getElementById(navItem)).style.backgroundColor="rgb(254, 166, 135)";
+    }
+  }
+
+  
+  hoverend(navItem:string){
+
+    console.log("hover");
+    if(navItem != this.selected){
+      (<HTMLInputElement>document.getElementById(navItem)).style.backgroundColor="white";
+    }
+  }
+
   deselect(){
-    (<HTMLInputElement>document.getElementById(this.selected)).style.backgroundColor="white";
-    (<HTMLInputElement>document.getElementById(this.selected)).style.color="rgb(82, 82, 82)";
+    console.log("deselect");
+
+    if(this.selected != null && this.selected != ''){
+      (<HTMLInputElement>document.getElementById(this.selected)).style.backgroundColor="white";
+      (<HTMLInputElement>document.getElementById(this.selected)).style.color="rgb(82, 82, 82)";
+    }
+    this.selected = '';
   }
 }
